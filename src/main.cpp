252a9a54d9c7f1913feb0d563406818cb80a17ff
hadi <hadi.niknam2004@gmail.com>
2024-06-30 14:24:38 +0430

    The unique Username added

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,9 @@ class Account{
 			this->username = username ; 
 			this->password = password ;
 		}	
+		string UsernameGetter(){
+			return username ;
+		}
 };
 
 void login()
@@ -30,7 +33,7 @@ void login()
 }
 void addDone(string &s,int refresh){
 	if(refresh){
-	s = s + "(Done)"; 
+		s = s + "(Done)"; 
 	}
 }
 void SendTodb(string name , string familyname , string username , string password ){
@@ -39,13 +42,12 @@ void SendTodb(string name , string familyname , string username , string passwor
 	db.push_back(Account(name,familyname, username, password) ) ; 
 }
 void signin_fields(int i, string(&field)[4], int(&flag)[5], string(&comp)[5]) {
-	// A Boosted Cin 
-	
+	// A Boosted Cin 	
 	int flager = 1 ; 
 	int refresh = 1 ; 
 	if(flag[i] == 1 ) { 
-		
-			CubbyMenu::Menu Y;
+
+		CubbyMenu::Menu Y;
 		Y.add_header("You had done this field do you want to edit it ?");
 		Y.add_item("Yes",[&refresh](){refresh = 0;});
 		Y.add_item("No",[&flager](){flager = 0 ;});
@@ -54,53 +56,65 @@ void signin_fields(int i, string(&field)[4], int(&flag)[5], string(&comp)[5]) {
 	if(flager) {
 		cout << "Enter Your " << field[i] << endl;
 		cin >> comp[i];
-		cout << "Done" << endl;
-		addDone(field[i],refresh);
-		flag[i] = 1;
+		int  flag2 = 1 ; 
+		if(i == 3 ){
+			// I want to check the existance of the username 
+			for(Account x : db ) {
+				if(comp[i].compare(x.UsernameGetter())){
+					flag2 = 0 ; 
+					break ; 
+				}
+			}
+		}
+			if(flag2) {
+				cout << "Done" << endl;
+				addDone(field[i],refresh);
+				flag[i] = 1;
+			}
+		}
 	}
-}
-// Attention : If you change the size of any array you must change the function input of signin_field as well 
-// component 
-string comp[5] ;
+	// Attention : If you change the size of any array you must change the function input of signin_field as well 
+	// component 
+	string comp[5] ;
 
-// check whether a field just complete or not...
-int flag[5] = {0} ; 
+	// check whether a field just complete or not...
+	int flag[5] = {0} ; 
 
-//The twitter fields
-string field[4]  ;
+	//The twitter fields
+	string field[4]  ;
 
-void SignUp(){
+	void SignUp(){
 
 
-	while(!flag[1] || !flag[2] || !flag[3] || !flag[4]){ 
-		CubbyMenu::Menu sign;
-		sign.add_header("----------SignUp Menu----------");
-		sign.add_item(field[1], [&]() {signin_fields(1,field,flag,comp) ; });
-		sign.add_item(field[2], [&]() { signin_fields(2,field,flag,comp) ; } );
-		sign.add_item(field[3], [&]() { signin_fields(3,field,flag,comp); } );
-		sign.add_item(field[4], [&]() {signin_fields(4,field,flag,comp) ; });
-		sign.print() ; 
+		while(!flag[1] || !flag[2] || !flag[3] || !flag[4]){ 
+			CubbyMenu::Menu sign;
+			sign.add_header("----------SignUp Menu----------");
+			sign.add_item(field[1], [&]() {signin_fields(1,field,flag,comp) ; });
+			sign.add_item(field[2], [&]() { signin_fields(2,field,flag,comp) ; } );
+			sign.add_item(field[3], [&]() { signin_fields(3,field,flag,comp); } );
+			sign.add_item(field[4], [&]() {signin_fields(4,field,flag,comp) ; });
+			sign.print() ; 
+		}
+
+		SendTodb(comp[1] , comp[2] ,comp[3] ,comp[4]) ; 
 	}
+	int main()
+	{
+		int exitcode = 1 ; 
+		field[1] = "Name" ;
+		field[2] = "FamilyName";
+		field[3] = "Username " ;
+		field[4] = "Password";
 
-	SendTodb(comp[1] , comp[2] ,comp[3] ,comp[4]) ; 
-}
-int main()
-{
-	int exitcode = 1 ; 
-	field[1] = "Name" ;
-	field[2] = "FamilyName";
-	field[3] = "Username " ;
-	field[4] = "Password";
+		while(exitcode){
+			CubbyMenu::Menu menu;
+			menu.add_header("---------AP-Twitter-Project---------");
 
-	while(exitcode){
-		CubbyMenu::Menu menu;
-		menu.add_header("---------AP-Twitter-Project---------");
-
-		menu.add_item("Login", &login );
-		menu.add_item("SignUp",&SignUp);
-		menu.add_item("Exit", [&exitcode](){ exitcode = 0 ;} );
+			menu.add_item("Login", &login );
+			menu.add_item("SignUp",&SignUp);
+			menu.add_item("Exit", [&exitcode](){ exitcode = 0 ;} );
 
 
-		menu.print();
+			menu.print();
+		}
 	}
-}
